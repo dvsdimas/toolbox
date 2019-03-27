d81339690d5be5423b0113f994edaa35fb300e81
Mylnev <Dmitry.Mylnev@Markets.com>
2019-03-27 23:06:07 +0300

    MaxPairwiseProduct first commit

--- src/main/java/APlusB.java
+++ src/main/java/APlusB.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class APlusB {
 
-    private static int sumOfTwoDigits(int first_digit, int second_digit) {
+    private static int sumOfTwoDigits(final int first_digit, final int second_digit) {
         return first_digit + second_digit;
     }
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
+
+        final Scanner s = new Scanner(System.in);
+
+        final int a = s.nextInt();
+
+        final int b = s.nextInt();
+
         System.out.println(sumOfTwoDigits(a, b));
     }
+
 }
